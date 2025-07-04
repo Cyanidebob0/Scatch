ba5b1c7e292b18029d20880f71d7b13ec1ddd222
bhuvan <bhuvanannappa@gmail.com>
2025-07-04 12:44:46 +0530

    added seperation of concerns

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const usermodel = require("./models/user-model");
 const productmodel = require("./models/product-model");
 const ownermodel = require("./models/owner-model");
 const db = require("./config/mongoose-connection");
+const usersRouter = require("./routes/usersRouters");
+const productsRouter = require("./routes/productsRouter");
+const ownersRouter = require("./routes/ownersRouter");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,9 +18,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookie());
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("yo");
-});
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/owners", ownersRouter);
 
 app.listen(3000, () => {
   console.log("Server is running");
