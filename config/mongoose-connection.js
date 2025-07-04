5c2cd6e85eaf1c8e6e7b6dc308fc948ba620b14d
bhuvan <bhuvanannappa@gmail.com>
2025-07-04 13:51:18 +0530

    git

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-
+const dbgr = require("debug")("development:mongoose");
+const config = require("config");
 mongoose
-  .connect("mongodb://127.0.0.1:27017/scatch_db")
+  .connect(`${config.get("MONGODB_URI")}/scatch_db`)
   .then(() => {
-    console.log("connect to db");
+    dbgr("connect to db");
   })
   .catch((err) => {
-    console.log(err);
+    dbgr(err);
   });
 
 module.export = mongoose.connection;
