d112f9c470a4635238587311079c802e1f152e14
bhuvan <bhuvanannappa@gmail.com>
2025-07-05 20:36:27 +0530

    Implement ted log out

--- routes/usersRouters.js
+++ routes/usersRouters.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const createUserController = require("../controllers/create-user-controller");
 const loginuserController = require("../controllers/login-user-controller");
+const logoutController = require("../controllers/logout-controller");
 
 router.post("/create", createUserController);
 
 router.post("/login", loginuserController);
+
+router.get("/logout", logoutController);
 
 router.get("/", (req, res) => {
   res.send("users");
