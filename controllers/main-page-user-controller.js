eee076c67553b6f77d6932098379c737e6ecb825
bhuvan <bhuvanannappa@gmail.com>
2025-07-08 16:42:36 +0530

    sort in both user and owner done

--- controllers/main-page-user-controller.js
+++ controllers/main-page-user-controller.js
@@ -2,5 +2,5 @@ const productModel = require("../models/product-model");
 
 module.exports = async (req, res) => {
   const products = await productModel.find();
-  res.render("mainpageuser", { products });
+  res.render("mainpageuser", { products ,sort:"price_asc"});
 };
