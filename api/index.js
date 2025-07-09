b236b08722cc39a7a37d1dfc31c0f69891067cee
bhuvan <bhuvanannappa@gmail.com>
2025-07-09 14:40:31 +0530

    mode osme changes

--- api/index.js
+++ api/index.js
@@ -1,33 +1,34 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../../.env" });
 const express = require("express");
 const app = express();
-const dbgr = require("debug")("development:main");
-
 const path = require("path");
+const flash = require("connect-flash");
+const cookieParser = require("cookie-parser");
 
-const db = require("../config/mongoose-connection");
 const usersRouter = require("./routes/usersRouters");
 const productsRouter = require("./routes/productsRouter");
 const ownersRouter = require("./routes/ownersRouter");
 const homeRouter = require("./routes/index");
-const flash = require("connect-flash");
-const cookieParser = require("cookie-parser");
-const sessionConfig = require("../config/session");
+
+const sessionConfig = require("../../config/session");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../../public")));
 app.set("view engine", "ejs");
-
 app.use(cookieParser());
 
 const sessionMiddleware = sessionConfig(app);
-
 app.use(flash());
 
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/owners", ownersRouter);
 app.use("/", homeRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 module.exports = app;
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
-const config = require("config");
 
-mongoose
-  .connect(`${config.get("MONGODB_URI")}/scatch_db`)
-  .then(() => {
-    dbgr("connect to db");
-  })
-  .catch((err) => {
-    dbgr(err);
-  }); 
-module.exports = mongoose.connection;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/scatch_db";
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    dbgr("MongoDB Connected...");
+    return mongoose.connection;
+  } catch (err) {
+    dbgr("MongoDB Connection Error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB();
